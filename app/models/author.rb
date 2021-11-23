f91eed207aba6c44d24de178993eeda5914044b9
conniekang <hqk1121@gmail.com>
2021-11-23 08:46:59 -0500

    commit tree 6031b071ef7d14a5539d1c33cd973d3cd851b51b

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,17 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+
+    def create
+        person = Author.create(person_params)
+        if person.valid?
+          render json: person, status: :created
+        else
+          render json: { error: person.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
 end
+
+
+
